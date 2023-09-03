03722441ab3f5a5bb92f441299e23e13a831a9c6
Roman Dmitriev <roman.dmitriev@dataart.com>
2023-09-03 10:43:32 +0300

    one big component

---
+++ content/characters.tsx
@@ -0,0 +1,70 @@
+import { images, sounds } from "./preloads";
+
+export const charactersData = [
+    {
+        name: 'killa',
+        image: images.KILLA,
+        onSuccessSounds: [
+            sounds.VLUDI,
+            sounds.ZAMERC,
+            sounds.PODARKOV,
+            sounds.PODGON,
+            ],
+    },
+    {
+        name: 'kot',
+        image: images.KOT,
+        onSuccessSounds: [
+            sounds.KANITEL,
+            sounds.ALE,
+            sounds.BUHOI,
+            sounds.SUHAR,
+            sounds.NATEME,
+            ],
+    },
+    {
+        name: 'oshparenni',
+        image: images.OSHPARENNI,
+        onSuccessSounds: [
+            sounds.TERPILA,
+            sounds.MERC,
+            sounds.BESPREDEL,
+            ],
+    },
+    {
+        name: 'rama',
+        image: images.RAMA,
+        onSuccessSounds: [
+            sounds.NEMI,
+            sounds.MOLOKA,
+            ],
+    },
+    {
+        name: 'eralash',
+        image: images.ERALASH,
+        onSuccessSounds: [
+            sounds.SMOTRI,
+            sounds.MICHISH,
+            sounds.GORODISH,
+            sounds.OBOSNUEM,
+            sounds.PROBLEM,
+            ],
+    },
+    {
+        name: 'sobachiha',
+        image: images.SOBACHIHA,
+        onSuccessSounds: [
+            sounds.POUBIVAUT,
+            sounds.KATAFALKA,
+            ],
+    },
+    {
+        name: 'tractorman',
+        image: images.TRACTORMAN,
+        onSuccessSounds: [
+            sounds.TR_PONYNA,
+            sounds.TR_NAZR1,
+            sounds.TR_NAZR2,
+            ],
+    },
+];
---
+++ content/common.tsx
@@ -0,0 +1,25 @@
+import {
+    CardsSettings,
+    MusicSettings,
+    PhrasesAndSymbols,
+} from "../tools/interfaces";
+import { images, sounds } from "./preloads";
+
+export const phrasesAndSymbols: PhrasesAndSymbols = {
+    restart: 'На тему сел?',
+    rightChocie: 'Ну ты коммерс!',
+    wrongChocie: 'Ты че буровишь, гандон?!!',
+};
+
+
+export const musicSettings: MusicSettings ={
+    mainThemeBackgroundMusic: sounds.MAIN,
+    greetingSoundList: [sounds.V_STOILO, sounds.ZAPRAVKA, sounds.DO_VIESNENIA]
+};
+
+export const cardsSettings: CardsSettings = {
+    cardBack: images.BOOMER,
+    mainActivityBackground: 'rgb(34, 41, 45)',
+    cardColor: 'rgb(68, 72, 98)',
+    buttonColor: 'rgb(68, 72, 98)',
+};
